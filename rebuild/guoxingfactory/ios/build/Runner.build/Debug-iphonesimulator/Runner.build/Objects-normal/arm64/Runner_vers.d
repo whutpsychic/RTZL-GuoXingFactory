b45f42574e0818868fe2f85a26738afa2118b9d6
瑞太智联 <1511828529@qq.com>
2024-02-07 16:40:20 +0800

    iOS17

---
+++ rebuild/guoxingfactory/ios/build/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Runner.build/Debug-iphonesimulator/Runner.build/DerivedSources/Runner_vers.c
---
+++ rebuild/guoxingfactory/ios/build/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/GeneratedPluginRegistrant.d
@@ -0,0 +1,20 @@
+dependencies: \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Modules/module.modulemap \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/Flutter.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterEngineGroup.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/ruitaizhilian/Documents/GitHub/RTZL-GuoXingFactory/rebuild/guoxingfactory/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterViewController.h
